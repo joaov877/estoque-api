b32db5d9f01dc8fb13a4b381490f7f231424e481
nk-medeiros <joaochagasnike@gmail.com>
2026-07-29 16:36:10 -0300

    feat: integra rotas de usuario na api

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 app.listen(3000, () => console.log('API rodando na porta 3000'));
+ // NOVA LINHA
+const app = express();
+app.use(express.json());
+app.use('/api/usuarios', userRoutes); // NOVA LINHA
+app.listen(3000, () => console.log('API rodando na porta 3000'));
