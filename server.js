359e2cae2389b5fa3cc282041315802a60646de3
joão victor <joaochagasnike@gmail.com>
2026-07-30 22:12:26 -0300

    feat: integra endpoint de status do estoque

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const userRoutes = require('./src/routes/userRoutes');
+const estoqueRoutes = require('./src/routes/estoqueRoutes'); // NOVA LINHA
+
 const app = express();
+
 app.use(express.json());
-app.listen(3000, () => console.log('API rodando na porta 3000'));
- // NOVA LINHA
-const app = express();
-app.use(express.json());
-app.use('/api/usuarios', userRoutes); // NOVA LINHA
+app.use('/api/usuarios', userRoutes);
+app.use('/api/estoque', estoqueRoutes); // NOVA LINHA
+
 app.listen(3000, () => console.log('API rodando na porta 3000'));
